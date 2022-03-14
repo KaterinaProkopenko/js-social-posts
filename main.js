65b3a0051f6ff6fd660a52835d85304ddc5f5fc5
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-03-14 18:25:17 +0100

    Aggiornamento ('mi piace' cambia il colore)

--- main.js
+++ main.js
@@ -56,9 +56,27 @@ const posts = [
     }
 ];
 
+let arrayLikes = [];
+
 posts.forEach((element) => {
     createPost(element.author['image'], element.author['name'], element.created, element.content, element.media, element.likes);
+    arrayLikes.push(element.likes);
 });
+
+const button = document.querySelectorAll('a.like-button');
+console.log(button);
+
+button.forEach((element) => {
+    console.log(element);
+    element.addEventListener('click', function() {
+        this.style.color = "red";
+        arrayLikes.forEach((element) => {
+            console.log(element + 1);
+        });
+    });
+});
+
+
 
 function createPost(imgProfile, name, date, text, img, likes) {
     const postContainer = document.createElement('div');
@@ -92,7 +110,7 @@ function createPost(imgProfile, name, date, text, img, likes) {
     postMetaAuthor.innerHTML += name;
 
     const postMetaTime = document.createElement('div');
-    postMetaAuthor.classList.add('post-meta__time');
+    postMetaTime.classList.add('post-meta__time');
     postMetaData.appendChild(postMetaTime);
     postMetaTime.innerHTML += date;
 
@@ -131,77 +149,6 @@ function createPost(imgProfile, name, date, text, img, likes) {
     likesJs.appendChild(likesCounter);
     likesCounter.innerHTML = `Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone`;
 };
-
-/* const postContainer = document.createElement('div');
-postContainer.classList.add('post');
-document.getElementById('container').appendChild(postContainer);
-
-const postHeader = document.createElement('div');
-postHeader.classList.add('post__header');
-postContainer.appendChild(postHeader);
-
-const postMeta = document.createElement('div');
-postMeta.classList.add('post-meta');
-postHeader.appendChild(postMeta);
-
-const postMetaIcon = document.createElement('div');
-postMetaIcon.classList.add('post-meta__icon');
-postMeta.appendChild(postMetaIcon);
-
-const imageProfile = document.createElement('img');
-imageProfile.classList.add('profile-pic');
-imageProfile.src = 'https://unsplash.it/300/300?image=15';
-postMetaIcon.appendChild(imageProfile);
-
-const postMetaData = document.createElement('div');
-postMetaData.classList.add('post-meta__data');
-postMeta.appendChild(postMetaData);
-
-const postMetaAuthor = document.createElement('div');
-postMetaAuthor.classList.add('post-meta__author');
-postMetaData.appendChild(postMetaAuthor);
-postMetaAuthor.innerHTML += 'Phil Mangione';
-
-const postMetaTime = document.createElement('div');
-postMetaAuthor.classList.add('post-meta__time');
-postMetaData.appendChild(postMetaTime);
-postMetaTime.innerHTML += '2021-06-25';
-
-const postText = document.createElement('div');
-postText.classList.add('post__text');
-postContainer.appendChild(postText);
-postText.innerHTML += 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.';
-
-const postImage = document.createElement('div');
-postImage.classList.add('post__image');
-postContainer.appendChild(postImage);
-
-const contentImage = document.createElement('img');
-contentImage.src = 'https://unsplash.it/600/300?image=171';
-postImage.appendChild(contentImage);
-
-const postFooter = document.createElement('div');
-postFooter.classList.add('post__footer');
-postContainer.appendChild(postFooter);
-
-const likesJs = document.createElement('div');
-likesJs.classList.add('likes', 'js-likes');
-postFooter.appendChild(likesJs);
-
-const likesCta = document.createElement('div');
-likesCta.classList.add('likes', 'js-likes');
-likesJs.appendChild(likesCta);
-likesCta.innerHTML = `
-<a class="like-button  js-like-button" href="#" data-postid="1">
-    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-    <span class="like-button__label">Mi Piace</span>
-</a>`;
-
-const likesCounter = document.createElement('div');
-likesCounter.classList.add('likes__counter');
-likesJs.appendChild(likesCounter);
-likesCounter.innerHTML = `Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone`; */
-
 
 
 
